d0f43342b8507567a03c46c1711161fb206c3338
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-01-13 20:00:37 +0200

    A short warning about static and concurrent write access; Fibonacci with memoization

--- Recursivity/Recursivity/Program.cs
+++ Recursivity/Recursivity/Program.cs
@@ -1,17 +1,76 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
 
 namespace Recursivity
 {
     class Program
     {
+        static int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        static void FindIndexOf10()
+        {
+            int element = 10;
+            int index = RecursivityHelper.IndexOfElement(element, array);
+
+            StringBuilder output = new StringBuilder();
+            output.AppendLine($"{DateTime.Now:HH:mm:ss:ffff}) Index of {element} = {index}");
+            output.AppendLine($"Index of {element} = {index}");
+
+            Console.WriteLine(output.ToString());
+        }
+
+        static void FindIndexOf2()
+        {
+            int element = 2;
+            int index = RecursivityHelper.IndexOfElement(element, array);
+
+            StringBuilder output = new StringBuilder();
+            output.AppendLine($"{DateTime.Now:HH:mm:ss:ffff}) Index of {element} = {index}");
+            output.AppendLine($"Index of {element} = {index}");
+
+            Console.WriteLine(output.ToString());
+        }
+
         static void Main(string[] args)
         {
-            int element = 1;
-            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int fibo = RecursivityHelper.Fibonacci(5);
+            Console.WriteLine(fibo);
 
+            Console.WriteLine("--------------------");
+
+            fibo = RecursivityHelper.FibonacciWithCache(5);
+            Console.WriteLine(fibo);
+
+            Console.WriteLine("--------------------");
+
+            fibo = RecursivityHelper.FibonacciWithCache(6);
+            Console.WriteLine(fibo);
+
+            /*
+             * Beware of multithreading race condition when writing static members!
+            Thread t1 = new Thread(FindIndexOf10);
+            Thread t2 = new Thread(FindIndexOf2);
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            int element = 10;
             int index = RecursivityHelper.IndexOfElement(element, array);
 
             Console.WriteLine($"Index of {element} = {index}");
+            Console.WriteLine($"Count of recursive calls: {RecursivityHelper.RecursiveIndexOfCounter}");
+
+            element = 2;
+            index = RecursivityHelper.IndexOfElement(element, array);
+
+            Console.WriteLine($"Index of {element} = {index}");
+            Console.WriteLine($"Count of recursive calls: {RecursivityHelper.RecursiveIndexOfCounter}");
+
+            */
         }
     }
 }
--- Recursivity/Recursivity/RecursivityHelper.cs
+++ Recursivity/Recursivity/RecursivityHelper.cs
@@ -6,6 +6,47 @@ namespace Recursivity
 {
     public static class RecursivityHelper
     {
+        private const int MaxFibonacciCacheSize = 100;
+        private static int[] FibonacciCache = new int[MaxFibonacciCacheSize];
+
+        public static int FibonacciWithCache(int n)
+        {
+            if (n >= 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(n), $"Param '{nameof(n)}' must be less than {MaxFibonacciCacheSize}");
+            }
+
+            if (n <= 1)
+            {
+                return n;
+            }
+             
+            if (FibonacciCache[n] > 0)
+            {
+                return FibonacciCache[n];
+            }
+            else
+            {
+                Console.WriteLine($"Called FibonacciWithCache({n})");
+
+                int fibo = FibonacciWithCache(n - 1) + FibonacciWithCache(n - 2);
+                FibonacciCache[n] = fibo;
+                return fibo;
+            }
+        }
+
+        public static int Fibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            Console.WriteLine($"Called Fibonacci({n})");
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
         public static int IndexOfElement(int element, int[] array)
         {
             return IndexOfElement(element, array, 0, array.Length - 1);
