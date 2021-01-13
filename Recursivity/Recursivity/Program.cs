6f43f8c52e9ad2bcad191c1f09047687391b6ca3
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-01-13 18:21:47 +0200

    Initial project setup

---
+++ Recursivity/Recursivity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recursivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
